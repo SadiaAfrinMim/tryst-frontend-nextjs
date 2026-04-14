2549ebc7b74ff236b0a208ec18cc7c9a40f30a02
Sadia Afrin Mim <mimsadia367@gmail.com>
2026-04-15 02:06:49 +0600

    almost done

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Tryst.link - Home of the independent escort',
+  description: 'Created with trust and safety at its core, Tryst.link is the fastest-growing escort platform in the world. We list thousands of independent adult entertainers, including escorts, BDSM, kink, video, massage, and much more.',
+  generator: 'Tryst.link',
   icons: {
     icon: [
       {
